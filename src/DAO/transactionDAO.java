55855eb58ffcf5b0a807f1eea904b075f8937789
Arch-qwerty <archforyll3306@gmail.com>
2026-07-10 19:17:17 +0800

    xawdawda

--- src/DAO/transactionDAO.java
+++ src/DAO/transactionDAO.java
@@ -5,8 +5,10 @@
 package DAO;
 
 import database.DBConnection;
+import java.awt.List;
 import java.math.BigDecimal;
 import java.sql.*;
+import java.util.ArrayList;
 import model.Expense;
 
 /**
@@ -17,7 +19,7 @@ public class transactionDAO {
 
     //add
     public void addExpense(Expense expense) throws SQLException {
-        String sql = "INSERT INTO transaction (category_name, amount, description) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO transaction (user_id, type, amount, category_id, description, transaction_date) VALUES (?, expense, ?, ?, ?, ?)";
 
         try (
                 Connection connection = DBConnection.getConnection(); PreparedStatement stmt = connection.prepareStatement(sql);) {
@@ -25,12 +27,15 @@ public class transactionDAO {
             stmt.setString(1, expense.getCategory());
             stmt.setBigDecimal(2, expense.getAmount());
             stmt.setString(3, expense.getDescription());
+            
             stmt.executeUpdate();
 
         }//body of try
 
     }
     //end of addExpense Method
+
+    //read
     
-    
+
 }
--- src/services/TransactionService.java
+++ src/services/TransactionService.java
@@ -10,33 +10,7 @@ public class TransactionService {
 
     
     //add
-    public boolean addTransaction(Transaction transaction) {
-
-        String sql = """
-                INSERT INTO transactions
-                (user_id, category_id, type,
-                 transaction_date, amount, description)
-                VALUES (?, ?, ?, ?, ?, ?)
-                """;
-
-        try (Connection conn = DBConnection.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-
-            ps.setInt(1, transaction.getUserId());
-            ps.setInt(2, transaction.getCategoryId());
-            ps.setString(3, transaction.getType());
-            ps.setDate(4, transaction.getTransactionDate());
-            ps.setBigDecimal(5, transaction.getAmount());
-            ps.setString(6, transaction.getDescription());
-
-            return ps.executeUpdate() > 0;
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return false;
-    }
+    
     
     //read all
     
@@ -45,8 +19,5 @@ public class TransactionService {
     
     //read one
     
-    
-    
-    /
 
 }
